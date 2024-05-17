e03c2f78762cc36ae21f3a3b2c6abcfc5bfdcc76
Chareeef <youssef.charif.h@gmail.com>
2024-05-17 16:11:58 +0000

    Set wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Our production launchpad!
+"""
+from main import create_app
+
+
+# Create and run app in production environment
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
