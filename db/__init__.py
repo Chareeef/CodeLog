005f9355d292391392ae6f96e7b4cf8cf9ed55f3
Chareeef <youssef.charif.h@gmail.com>
2024-05-18 17:58:11 +0000

    feat: create a redis client

--- db/__init__.py
+++ db/__init__.py
@@ -2,6 +2,7 @@
 """Initialize MongoDB client
 """
 from db.db_manager import DBStorage
+from db.redis_client import redis_client
 import os
 
 db = DBStorage()
---
+++ db/redis_client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Create a Redis client
+"""
+import redis
+import os
+
+
+if os.getenv('MODE') == 'TEST':
+    db_num = 1
+else:
+    db_num = 0
+
+# Create Redis client
+redis_client = redis.Redis(host='localhost', port=6379, db=db_num)
