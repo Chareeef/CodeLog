69a334e96c841bb112c6b22f629943038c128cac
Gdija <khadijaghadi00@gmail.com>
2024-05-21 01:02:28 +0100

    added react app

---
+++ react_app/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+ const Home = () => {
+  return (
+    <div className='home-container'>
+    <Navigation />
+    </div>
+  )
+}
+export default Home; 
