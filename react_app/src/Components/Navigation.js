9485cd31d9680bc4c3c376ab6eafab7df428c16f
MwendeHarriet <mwendeharriet@gmail.com>
2024-05-28 01:09:23 +0300

    feat: Added links to other components on navbar

--- react_app/src/Components/Navigation.js
+++ react_app/src/Components/Navigation.js
@@ -33,8 +33,19 @@ const Navigation = () => {
   return (
     <nav className='nav'>
       <a href='/' className='site-title'>
-
         SoftwareSphere
+      </a>
+      <a href='/' className='site-navitem1 font-lg hover:font-xl hover:text-purple-700'>
+        Posts
+      </a>
+      <a href='/' className='site-navitem2 font-lg hover:font-xl hover:text-purple-700'>
+        Longest   Streak
+      </a>
+      <a href='/' className='site-navitem3 font-lg hover:font-xl hover:text-purple-700'>
+        Debug   Corner
+      </a>
+      <a href='/' className='site-navitem4 font-lg hover:font-xl hover:text-purple-700'>
+        Profile
       </a>
       <div className='navbar-links-container'>
         <ul className={showMenu ? 'open' : ''}>
