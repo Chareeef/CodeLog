f3f5027cc08abaec1769b35a1a08886b728da2b2
Chareeef <youssef.charif.h@gmail.com>
2024-07-26 17:58:02 +0000

    fix sizes

--- react_frontend/src/Components/AnimationSecTwo.js
+++ react_frontend/src/Components/AnimationSecTwo.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Community from '../Assets/images/community.jpg';
 import '../index.css';
-import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
 function AnimationSecTwo() {
   return (
     <figure className='relative flex justify-center items-center h-full'>
       <img
-        className='h-3/3 w-9/12 rounded-xl object-cover object-center mt-2'
+        className='community-img rounded-xl object-fit object-center mt-2 '
         src={Community}
         alt='community'
       />
-      <figcaption className='absolute w-50 flex flex-col justify-center items-center p-5 rounded-xl border border-white bg-white/75 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm'>
-        <Typography
-          variant='h1'
-          fontWeight='bold'
-          className='text-center font-bold text-green mb-4'
+      <figcaption className='absolute flex flex-col justify-center items-center mx-5 p-5 rounded-xl border border-white bg-white/75 shadow-lg shadow-black/5 saturate-200'>
+        <h1
+          className='text-center display-1 font-bold text-green mb-4'
         >
           Join A Wonderful Community
-        </Typography>
+        </h1>
         <Link
           to='/register'
           className='p-2 text-white font-semibold text-lg hover:underline bg-green border border-black rounded'
